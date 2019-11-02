a26a6112ba931a21beda373c371f6ff122e0694c
Marlon <marlonjomar07@gmail.com>
2019-11-02 01:26:38 -0600

    Diseño de las interfaces

--- app/src/main/java/com/example/calculadora/Calculadora.java
+++ app/src/main/java/com/example/calculadora/Calculadora.java
@@ -3,12 +3,18 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class Calculadora extends AppCompatActivity {
+public class Calculadora extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
